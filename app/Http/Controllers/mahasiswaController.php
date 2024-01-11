f8b4ba5541a37218fcec22a3b737edd0d5c80350
unknown <sakinaharbaiyah73@gmail.com>
2024-01-11 16:13:09 +0700

    project pertama saya di laravel

---
+++ app/Http/Controllers/mahasiswaController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class mahasiswaController extends Controller
+{
+    function index() {
+        return'hallo world';
+    }
+}
+{
+    function home() {
+        return'home';
+    }
+}
